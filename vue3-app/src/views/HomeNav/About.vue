3c7f14660572337a6729a351bfce75059c5d1f15
gNat <gNat@com>
2023-05-09 17:12:42 +0800

    样式第一次提交

--- vue3-app/src/views/HomeNav/About.vue
+++ vue3-app/src/views/HomeNav/About.vue
@@ -1,15 +1,31 @@
 <template>
-    <div>
-        About
-    </div>
+  <div>
+    <header>
+      <h1>我的漫想家</h1>
+    </header>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {}
 </script>
 
 <style lang="scss" scoped>
-
+div {
+    background-color: #f4f5f5;
+    width: 100vw;
+    height: 100vh;
+  header {
+    width: 100%;
+    background-color: #18202d;
+    height: 35vh;
+    padding-top: 20rem;
+    h1 {
+      text-align: center;
+      font-size: 16rem;
+      color: #f5f5f6;
+      
+    }
+  }
+}
 </style>
--- vue3-app/src/views/HomeNav/HomeNav.vue
+++ vue3-app/src/views/HomeNav/HomeNav.vue
@@ -1,24 +1,44 @@
 <template>
-  <div>
+  <div class="box">
     <div>
       <router-view></router-view>
     </div>
     <nav>
-      <router-link to="/" >漫想家</router-link>
-      <router-link to="community" >社区</router-link>
-      <router-link  to="pointShopping">积分商城</router-link>
-      <router-link to="shoppingCart" >购物袋</router-link>
+      <router-link to="/">漫想家</router-link>
+      <router-link to="community">社区</router-link>
+      <router-link to="pointShopping">积分商城</router-link>
+      <router-link to="shoppingCart">购物袋</router-link>
       <router-link to="about">我的</router-link>
     </nav>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {}
 </script>
 
 <style lang="scss" scoped>
-
+.box {
+  // background-color: pink;
+  height: 100vh;
+  width: 100vw;
+  nav {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 70rem;
+    background-color: #ffffff;
+    a {
+      color: #cfcfd4;
+      font-weight: bold;
+      &.router-link-exact-active {
+        color: #404040;
+      }
+    }
+  }
+}
 </style>
