55b32eff1fc30be1bf44a5ad8cac43c4aeffcf15
eneo <eneo@.com>
2023-05-15 14:26:18 +0800

    时间戳函数

---
+++ vue3-app/src/js/tool.js
@@ -0,0 +1,10 @@
+/* 时间戳转换为时间 */
+export function timestampToTime(timestamp) {
+    timestamp = timestamp ? timestamp : null;
+    let date = timestamp;//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+    let Y = date.getFullYear() + '-';
+    let M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+    let D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' ';
+    
+    return Y + M + D 
+  }
